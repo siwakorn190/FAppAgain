04363e614010acf4bc86c9a49873480246609c5a
Siwakorn <demon.evil.30@gmail.com>
2020-08-26 16:44:37 +0700

    Change in fragment_contact.xml at the titleFragment to layout and Add FrameLayout in cover TextView

--- app/src/main/java/com/example/fappagain/AboutmeFragment.kt
+++ app/src/main/java/com/example/fappagain/AboutmeFragment.kt
@@ -8,16 +8,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.fappagain.databinding.FragmentAboutmeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AboutmeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AboutmeFragment : Fragment() {
 
     override fun onCreateView(
