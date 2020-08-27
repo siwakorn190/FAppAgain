68fcf88eec2e92e78caf4e33329cf122898eb02c
Siwakorn <demon.evil.30@gmail.com>
2020-08-27 20:27:09 +0700

    Update Code ContactFragment at function onCreateView

--- app/src/main/java/com/example/fappagain/ContactFragment.kt
+++ app/src/main/java/com/example/fappagain/ContactFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.fappagain.database.Database
 import com.example.fappagain.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
@@ -14,14 +16,25 @@ class ContactFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         var binding = DataBindingUtil.inflate<FragmentContactBinding>(
             inflater,
             R.layout.fragment_contact,
             container,
             false
         )
+
         setHasOptionsMenu(true)
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = Database.getInstance(application).databaseDao
+        val viewModelFactory = ContactViewModelFactory(dataSource, binding, application)
+        val contactViewModel =
+            ViewModelProvider(
+                this, viewModelFactory
+            ).get(ContactViewModel::class.java)
+        binding.contactViewModel = contactViewModel
+        binding.lifecycleOwner = this
         return binding.root
     }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
